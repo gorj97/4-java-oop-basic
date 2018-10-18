c2de5121e54b708c63fc06d99506874abf5f4a5c
gorj97 <gorj97@mail.ru>
2018-10-18 23:41:36 +0500

    Капров Егор Задание №1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,31 @@ public class Point {
     int x;
     int y;
 
+    Point(){    }
+
+    Point(int X, int Y){
+        this.x = X;
+        this.y = Y;
+    }
+
+    void flip(){
+        int tmp = this.x;
+        this.x = this.y - 2 * this.y;
+        this.y = tmp - 2*tmp;
+    }
+
+    double distance(Point point){
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+    }
+
+    public String toString(){
+        return "(" + String.valueOf(this.x) + ", " + String.valueOf(this.y) + ")";
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+
+
 }
