e910a126e5ae3a71fbf5a642c4073028ce6df304
gorj97 <gorj97@mail.ru>
2018-10-19 17:02:01 +0500

    Карпов Егор Задание №5

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,17 +1,30 @@
 package com.example.task05;
 
+import java.util.*;
+
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+
+    private List<Point> arrPoints = new ArrayList<Point>();
+
+    public PolygonalLine(){}
+
+    public void setPoints(Point[] points) {
+        this.arrPoints.clear();
+        for (Point point : points) {
+            this.arrPoints.add(new Point(point.getX(), point.getY()));
+        }
+    }
 
     /**
      * Устанавливает точки ломаной линии
      *
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
-    public void setPoints(Point[] points) {
-        // TODO: реализовать
+    public PolygonalLine(Point[] points) {
+        this.setPoints(points);
     }
 
     /**
@@ -20,7 +33,7 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        this.arrPoints.add(new Point(point.getX(), point.getY()));
     }
 
     /**
@@ -30,7 +43,7 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        this.arrPoints.add(new Point(x, y));
     }
 
     /**
@@ -39,8 +52,16 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double result = 0;
+        Iterator<Point> i = arrPoints.iterator();
+        Point point1 = i.next();
+        Point point2;
+        while (i.hasNext()) {
+            point2 = i.next();
+            result += point1.getLength(point2);
+            point1 = point2;
+        }
+        return result;
     }
 
 }
